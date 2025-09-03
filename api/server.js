7db6802f55aff919a2d03bd0824e44be5f78b41f
Vladyslav Krut <krut.vladyslav@gmail.com>
2025-09-03 19:28:38 +0200

    Added infrastructure skeleton

---
+++ api/server.js
@@ -0,0 +1,17 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+
+// Enable CORS for all routes
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', '*')
+  next()
+})
+
+server.use(router)
+
+module.exports = server
